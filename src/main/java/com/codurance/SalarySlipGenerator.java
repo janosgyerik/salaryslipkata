80c0fdba80e645d38757327382d1d31821c44e52
Pierre-Jean Baraud <pierre-jean@baraud.fr>
2018-02-28 20:10:36 +0000

    Pass test

--- src/main/java/com/codurance/SalarySlipGenerator.java
+++ src/main/java/com/codurance/SalarySlipGenerator.java
@@ -11,11 +11,16 @@ public class SalarySlipGenerator {
         return new SalarySlip(employee.id(), employee.name(),
                 monthlyValue(employee.grossSalary()),
                 monthlyValue(computeNationalInsuranceContribution(employee.grossSalary())),
-                new BigDecimal("916.67"), new BigDecimal("83.33"));
+                new BigDecimal("916.67"), computeTaxableIncome(employee.grossSalary()));
+    }
+
+    private BigDecimal computeTaxableIncome(BigDecimal annualGrossSalary) {
+        BigDecimal taxableAmount = annualGrossSalary.subtract(new BigDecimal("11000.00"));
+        return monthlyValue(taxableAmount);
     }
 
     private BigDecimal monthlyValue(BigDecimal yearlyValue) {
-        return yearlyValue.divide(new BigDecimal("12"), 2, RoundingMode.CEILING);
+        return yearlyValue.divide(new BigDecimal("12"), 2, RoundingMode.HALF_UP);
     }
 
     private static BigDecimal computeNationalInsuranceContribution(BigDecimal yearlySalary) {
